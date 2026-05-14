dcaa3c01e413c49d932fe077a37f0a7fbead53b7
Tikud11 <126788105+Tikud11@users.noreply.github.com>
2026-05-14 20:00:56 +0300

    Add files via upload

---
+++ js/anchor.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const offset = window.innerWidth >= 768 ? 80 : 40;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
